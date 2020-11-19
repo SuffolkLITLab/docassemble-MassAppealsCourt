e676e3d8960d3da0d99daa9ac20e3a3263035e70
Mia Bonardi <65858862+miabonardi@users.noreply.github.com>
2020-11-19 12:40:30 -0500

    Commit contact information other method

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.MassAppealsCourt',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.MassAppealsCourt\n\nA docassemble extension.\n\n## Author\n\nSystem Administrator, admin@admin.com\n\n',
+      long_description='# docassemble.MassAppealsCourt\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='System Administrator',
       author_email='admin@admin.com',
